8b4cd90627223915f821b45bb8a654ce8b3882f1
Gabriel Montplaisir <gabriel.montplaisir@protonmail.com>
2024-08-06 19:04:04 -0400

    Updated comments and fixed string literals

--- app/src/main/java/com/example/nasaimageoftheday/MainActivity.java
+++ app/src/main/java/com/example/nasaimageoftheday/MainActivity.java
@@ -2,15 +2,12 @@ package com.example.nasaimageoftheday;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -25,7 +22,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main); // Overriden by BaseActivity
+        setContentView(R.layout.activity_main); // Overridden by BaseActivity
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
